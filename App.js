503cbc1b083a46bcf0c9520a7a90d9a8a9965b8c
Matthias Holzer <ma.holzer@syskron.com>
2018-05-23 07:36:22 +0200

    test add entry

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
+import addEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Opentest up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <AddEntry />
       </View>
     );
   }
---
+++ components/AddEntry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Text, View} from 'react-native'
+
+import PropTypes from 'prop-types'
+
+export default class AddEntry extends Component {
+  render() {
+    return (
+      <View>
+        <Text>AddEntry</Text>
+      </View>
+    )
+  }
+}
